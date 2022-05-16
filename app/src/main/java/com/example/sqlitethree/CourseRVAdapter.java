b39e87c744cd1cdc77479d79faa15311ffecf7fd
janetbabirye <jbabirye2020@gmail.com>
2022-05-16 15:30:04 +0100

    first commit

---
+++ app/src/main/java/com/example/sqlitethree/CourseRVAdapter.java
@@ -0,0 +1,67 @@
+package com.example.sqlitethree;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CourseRVAdapter extends RecyclerView.Adapter<CourseRVAdapter.ViewHolder> {
+
+    // variable for our array list and context
+    private final ArrayList<CourseModel> courseModalArrayList;
+
+    // constructor
+    public CourseRVAdapter(ArrayList<CourseModel> courseModalArrayList, Context context) {
+        this.courseModalArrayList = courseModalArrayList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // on below line we are inflating our layout
+        // file for our recycler view items.
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.course_rv_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // on below line we are setting data
+        // to our views of recycler view item.
+        CourseModel modal = courseModalArrayList.get(position);
+        holder.courseNameTV.setText(modal.getCourseName());
+        holder.courseDescTV.setText(modal.getCourseDescription());
+        holder.courseDurationTV.setText(modal.getCourseDuration());
+        holder.courseTracksTV.setText(modal.getCourseTracks());
+    }
+
+    @Override
+    public int getItemCount() {
+        // returning the size of our array list
+        return courseModalArrayList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // creating variables for our text views.
+        private final TextView courseNameTV;
+        private final TextView courseDescTV;
+        private final TextView courseDurationTV;
+        private final TextView courseTracksTV;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            // initializing our text views
+            courseNameTV = itemView.findViewById(R.id.idTVCourseName);
+            courseDescTV = itemView.findViewById(R.id.idTVCourseDescription);
+            courseDurationTV = itemView.findViewById(R.id.idTVCourseDuration);
+            courseTracksTV = itemView.findViewById(R.id.idTVCourseTracks);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sqlitethree/ViewCourses.java
@@ -0,0 +1,39 @@
+package com.example.sqlitethree;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ViewCourses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_courses);
+
+        // initializing our all variables.
+        // creating variables for our array list,
+        // dbhandler, adapter and recycler view.
+        ArrayList<CourseModel> courseModalArrayList = new ArrayList<>();
+        DBHandler dbHandler = new DBHandler(ViewCourses.this);
+
+        // getting our course array
+        // list from db handler class.
+        courseModalArrayList = dbHandler.readCourses();
+
+        // on below line passing our array lost to our adapter class.
+        CourseRVAdapter courseRVAdapter = new CourseRVAdapter(courseModalArrayList, ViewCourses.this);
+        RecyclerView coursesRV = findViewById(R.id.idRVCourses);
+
+        // setting layout manager for our recycler view.
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ViewCourses.this, RecyclerView.VERTICAL, false);
+        coursesRV.setLayoutManager(linearLayoutManager);
+
+        // setting our adapter to recycler view.
+        coursesRV.setAdapter(courseRVAdapter);
+    }
+}
